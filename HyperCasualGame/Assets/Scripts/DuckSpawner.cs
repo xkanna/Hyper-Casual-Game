eee06b3ab2d18009cc42aa6c225765fbbcb1ed93
Katarina Veljkovic <40143205+xkanna@users.noreply.github.com>
2022-08-31 22:00:11 +0200

    Play and pause UI added

--- HyperCasualGame/Assets/Scripts/DuckSpawner.cs
+++ HyperCasualGame/Assets/Scripts/DuckSpawner.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DuckSpawner : MonoBehaviour
 {
     public Ducks ducks;
     public DuckPositions duckPositions;
     private int tapCounter;
+    private int mergeCounter;
     private int type1DuckCounter;
     private int type2DuckCounter;
     private int type3DuckCounter;
     private int type4DuckCounter;
     private int type5DuckCounter;
     public List<GameObject> spawnedDucks;
+    public TextMeshProUGUI numberOfTapsText;
+    public TextMeshProUGUI numberOfMergesText;
 
     void Start()
     {
@@ -29,15 +33,16 @@ public class DuckSpawner : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            if(tapCounter < 10)
+            tapCounter++;
+            if(type1DuckCounter < 10)
             {
                 GameObject newDuckType1 = Instantiate(ducks.duckPrefabs[0], duckPositions.possibleDuckPositions[spawnedDucks.Count], Quaternion.Euler(new Vector3(-90, 0, -90)));
-                tapCounter++;
+                type1DuckCounter++;
                 spawnedDucks.Add(newDuckType1);
             }
-            else if (tapCounter == 10)
+            else if (type1DuckCounter == 10)
             {
-                tapCounter = 0;
+                type1DuckCounter = 0;
                 
                 for(int i = 0; i < 10; i++)
                 {
@@ -48,7 +53,7 @@ public class DuckSpawner : MonoBehaviour
                 GameObject newDuckType2 = Instantiate(ducks.duckPrefabs[1], duckPositions.possibleDuckPositions[spawnedDucks.Count], Quaternion.Euler(new Vector3(-90, 0, -90)));
                 type2DuckCounter++;
                 spawnedDucks.Add(newDuckType2);
-
+                mergeCounter++;
             }
 
             if (type2DuckCounter == 10)
@@ -64,6 +69,7 @@ public class DuckSpawner : MonoBehaviour
                 type3DuckCounter++;
                 GameObject newDuckType3 = Instantiate(ducks.duckPrefabs[2], duckPositions.possibleDuckPositions[spawnedDucks.Count], Quaternion.Euler(new Vector3(-90, 0, -90)));
                 spawnedDucks.Add(newDuckType3);
+                mergeCounter++;
             }
 
             if (type3DuckCounter == 10)
@@ -79,6 +85,7 @@ public class DuckSpawner : MonoBehaviour
                 type4DuckCounter++;
                 GameObject newDuckType4 = Instantiate(ducks.duckPrefabs[3], duckPositions.possibleDuckPositions[spawnedDucks.Count], Quaternion.Euler(new Vector3(-90, 0, -90)));
                 spawnedDucks.Add(newDuckType4);
+                mergeCounter++;
             }
 
             if (type4DuckCounter == 10)
@@ -94,7 +101,15 @@ public class DuckSpawner : MonoBehaviour
                 type5DuckCounter++;
                 GameObject newDuckType5 = Instantiate(ducks.duckPrefabs[4], duckPositions.possibleDuckPositions[spawnedDucks.Count], Quaternion.Euler(new Vector3(-90, 0, -90)));
                 spawnedDucks.Add(newDuckType5);
+                mergeCounter++;
             }
         }
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        numberOfTapsText.text = tapCounter.ToString();
+        numberOfMergesText.text = mergeCounter.ToString();
     }
 }
